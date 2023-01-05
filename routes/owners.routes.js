b7ff5b0801730020d7ed14aa68d2f5ad54bc8fb5
younghwan90s <younghwan1608@gmail.com>
2023-01-06 02:57:10 +0900

    no message

--- routes/owners.routes.js
+++ routes/owners.routes.js
@@ -6,19 +6,19 @@ const OwnersController = require('../controllers/owners.controller');
 const ownersController = new OwnersController();
 
 // 사장 메인페이지
-router.get('/owner/', ownerMiddleware, ownersController.getOwnerPoints)
+router.get('/owner/', ownersController.getOwnerPoints)
 // 세탁서비스조회
-router.get('/owner/laundry_list', ownerMiddleware, ownersController.getLaundries);
+router.get('/owner/laundry_list', ownersController.getLaundries);
 // 세탁서비스조회:수거하기
-router.post('/owner/laundry_list/:laundryId/add', ownerMiddleware, ownersController.addToMyWorks); 
+router.post('/owner/laundry_list/:laundryId/add', ownersController.addToMyWorks); 
 // 작업 상태변경
-router.put('/owner/ownerWorkList/:laundryId/edit', ownerMiddleware, ownersController.updateLaundryStatus);
+router.put('/owner/ownerWorkList/:laundryId/edit', ownersController.updateLaundryStatus);
 // 작업내역조회
-router.get('/owner/ownerWorkList', ownerMiddleware, ownersController.getMyWorks);
+router.get('/owner/ownerWorkList', ownersController.getMyWorks);
 // 리뷰 조회
-router.get('/owner/ownerReview', ownerMiddleware, ownersController.getMyReviews);
+router.get('/owner/ownerReview', ownersController.getMyReviews);
 // 작업취소하기
-router.delete('/owner/ownerWorkList/:laundryId/delete', ownerMiddleware, ownersController.deleteWork);
+router.delete('/owner/ownerWorkList/:laundryId/delete', ownersController.deleteWork);
 
 
 module.exports = router;
