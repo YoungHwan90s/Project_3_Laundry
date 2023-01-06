391fd24eec4479b54c65ae7c68246e839825c3e7
younghwan90s <younghwan1608@gmail.com>
2023-01-06 13:12:29 +0900

    no message

--- app.js
+++ app.js
@@ -7,16 +7,6 @@ app.use(express.json());
 app.use('/api', express.urlencoded({ extended: false }), router);
 app.use(express.static("assets"));
 
-require("dotenv").config();
-
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.static(path.join(__dirname, 'views')))
-
-
-app.get('/usermain', (req, res) => {
-  res.render('user.ejs');
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`${process.env.PORT} 포트가 열렸습니다!!^^`);
