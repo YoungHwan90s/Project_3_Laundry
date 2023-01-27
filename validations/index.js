4528002857f90c14a808e643366022759cb92964
Younghwan90s <younghwan1608@gmail.com>
2023-01-27 16:15:15 +0900

    no message

--- validations/index.js
+++ validations/index.js
@@ -11,16 +11,23 @@ const signupValidation = Joi.object({
   confirmPwd: Joi.equal(Joi.ref('pwd')).required().messages({
     'any.only': '비밀번호가 일치하지 않습니다.',
   }),
-  ownerEmail: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }).required().messages({
-    'string.email': '이메일 형식에 맞지 않습니다.',
-  }),
-  ownerPhone:Joi.string().length(13).pattern(/^\d{3}-\d{4}-\d{4}$/).required().messages({
-    'string.length': '휴대폰번호는 (-)를 포함하여 13자리입니다.',
-    'string.pattern.base': '휴대폰번호 형식에 맞게 입력하십시오',
-  }),
+  ownerEmail: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required()
+    .messages({
+      'string.email': '이메일 형식에 맞지 않습니다.',
+    }),
+  ownerPhone: Joi.string()
+    .length(13)
+    .pattern(/^\d{3}-\d{4}-\d{4}$/)
+    .required()
+    .messages({
+      'string.length': '휴대폰번호는 (-)를 포함하여 13자리입니다.',
+      'string.pattern.base': '휴대폰번호 형식에 맞게 입력하십시오',
+    }),
   ownerAddress: Joi.string().required().messages({
     'string.empty': '업제주소는 필수 입력 사항입니다.',
   }),
